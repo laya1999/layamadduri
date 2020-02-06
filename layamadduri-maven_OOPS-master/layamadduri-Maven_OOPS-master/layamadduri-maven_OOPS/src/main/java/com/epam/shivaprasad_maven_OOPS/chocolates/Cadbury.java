8709e557e204a534e8d8fe208f4df0c74856abd8
laya1999 <51361376+laya1999@users.noreply.github.com>
2020-02-06 18:39:20 +0530

    Add files via upload

---
+++ layamadduri-maven_OOPS-master/layamadduri-Maven_OOPS-master/layamadduri-maven_OOPS/src/main/java/com/epam/shivaprasad_maven_OOPS/chocolates/Cadbury.java
@@ -0,0 +1,8 @@
+package com.epam.layamadduri_maven_OOPS.chocolates;
+
+public class Cadbury extends Chocolate {
+
+    public Cadbury(String name,int price,int weight){
+        super(name,price,weight);
+    }
+}
